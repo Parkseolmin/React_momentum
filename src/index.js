3a68cfe36d87cb55735adcc9186ead52dcf6336d
Parkseolmin <qkrtjfals2@naver.com>
2024-12-06 17:04:54 +0900

    QueryClient defaultOptions update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  keepPreviousData,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { OptionProvider } from 'context/OptionContext';
 import { HelmetProvider } from 'react-helmet-async';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+      placeholderData: keepPreviousData,
+      onError: (error) => console.error('Query Error:', error),
+      onSuccess: (data) => console.log('Query Success:', data),
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
